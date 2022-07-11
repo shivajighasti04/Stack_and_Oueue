8fdaae1e892f73c8d27180df74bfdb95cf8c7953
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-11 20:33:01 +0530

    Program to create a Stack

---
+++ StackMain.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.stackandqueue;
+
+import java.util.Scanner;
+
+/**
+ *  Program to stack implementation push operation
+ *  
+ * @author Shivaji Ghasti
+ * 
+ */
+public class StackMain {
+	public static void main(String[] args) {
+		System.out.println("------- Welcome to Stacks and Queues Program ---------");
+		System.out.println();
+
+		 StackImplimentation stackImplementation = new  StackImplimentation();
+		
+		// Calling operation menu method of StackImplementation
+		stackImplementation.operationsMenu();
+	}
+}
+	
+
+
+
