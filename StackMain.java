ad4332c9ef592538858ae1b69dab97ad68698c8e
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-11 20:43:06 +0530

    Program to peek and pop elements of stack

---
+++ StackMain.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.stackandqueue;
+
+import java.util.Scanner;
+
+/**
+ * Ability to peak and pop from the Stack till it is empty
+ *  
+ * @author Shivaji
+ * 
+ */
+public class StackMain {
+	public static void main(String[] args) {
+		
+		System.out.println("------- Welcome to Stacks and Queues Program ---------");
+		System.out.println();
+
+		StackImplimentation stackImplementation = new StackImplimentation();
+
+		// Calling operation menu method of StackImplementation
+		stackImplementation.operationsMenu();
+	}
+}
+		
+
+	
+
+
+
