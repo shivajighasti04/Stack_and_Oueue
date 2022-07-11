a5c1532e91afa88e96337a33180ea2c38d563770
shivajighasti/04 <shivajighasti97@gmail.com>
2022-07-11 20:42:39 +0530

    Added Peek and Pop Functions

---
+++ StackImplimentation.java
@@ -0,0 +1,104 @@
+package com.bridgelabz.stackandqueue;
+
+import java.util.Scanner;
+
+/**
+ * Stack operation implementation
+ * 
+ * @author Shivaji
+ * 
+ *
+ */
+public class StackImplimentation {
+	
+	Node top;
+
+	// Operation menu to take user choice of operation
+	public void operationsMenu() {
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+
+		do {
+			System.out.println("Operations menu : ");
+			System.out.println("1. To display \n2. Push Element  \n3. Pop Element \n4. Peek Element \n5. To exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Display queue elements : ");
+				display();
+				System.out.println("------------------------------------------------------\n");
+				break;
+			case 2:
+				System.out.println("Enter element to insert into the queue : ");
+				int element = sc.nextInt();
+				push(element);
+				System.out.println("------------------------------------------------------\n");
+				break;
+			case 3:
+				pop();
+				System.out.println("------------------------------------------------------\n");
+				break;
+			case 4:
+				System.out.println("Top element : " + peek());
+				System.out.println("------------------------------------------------------\n");
+				break;
+			default:
+				flag = false;
+				System.out.println("Thank you !");
+				System.out.println("------------------------------------------------------\n");
+				break;
+			}
+		} while (flag);
+	}
+
+	// Display method to display stack elements
+	public void display() {
+
+		if (top == null)
+			System.out.println("Stack is empty !");
+		else {
+			Node temp = top;
+			while (temp.next != null) {
+				System.out.print(temp.data + "  ");
+				temp = temp.next;
+			}
+			System.out.print(temp.data + "  ");
+			System.out.println();
+		}
+	}
+
+	// Push method to take input element in stack
+	public void push(int item) {
+		Node node = new Node();
+		node.data = item;
+		if (node == null) {
+			node.next = null;
+		} else {
+			node.next = top;
+			top = node;
+		}
+	}
+
+	// Pop method to remove element
+	public void pop() {
+		if (top == null) {
+			System.out.println("Stack is empty.");
+		} else {
+			Node temp = top;
+			System.out.println("Removing : " + temp.data);
+			top = top.next;
+		}
+	}
+
+	// Peek method to peek top element
+	public int peek() {
+		if (top != null) {
+			return top.data;
+		} else {
+			System.out.println("Stack is empty.");
+			return -1;
+		}
+	}
+}
+
+
